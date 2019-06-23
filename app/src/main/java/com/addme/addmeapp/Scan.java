166c266a1b3d2d4df46a12f2449ab4f266c7870f
mlazana <mariettalaz98@gmail.com>
2019-06-23 20:05:48 +0300

    Update Scan.java

--- app/src/main/java/com/addme/addmeapp/Scan.java
+++ app/src/main/java/com/addme/addmeapp/Scan.java
@@ -1,21 +1,61 @@
 package com.addme.addmeapp;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toolbar;
+import android.support.design.widget.TabLayout;
 
 public class Scan extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("My QR Code"));
+        tabLayout.addTab(tabLayout.newTab().setText("Scan"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        ScanFragments scanFragments = new ScanFragments(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(scanFragments);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
 
         TextView title = (TextView) findViewById(R.id.scanTitle);
         title.setText("This is Scan");
@@ -23,6 +63,7 @@ public class Scan extends AppCompatActivity {
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
+
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -49,4 +90,5 @@ public class Scan extends AppCompatActivity {
             }
         });
     }
+
 }
